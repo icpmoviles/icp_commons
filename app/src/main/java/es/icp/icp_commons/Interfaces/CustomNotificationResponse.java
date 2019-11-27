4253dcaa1f23f35bcc2cc7552e1d14578644753d
Ventura De Lucas <ventura.delucas@icp.local>
2019-11-27 09:30:03 +0100

    CustomNotification

---
+++ app/src/main/java/es/icp/icp_commons/Interfaces/CustomNotificationResponse.java
@@ -0,0 +1,7 @@
+package es.icp.icp_commons.Interfaces;
+
+import es.icp.icp_commons.CustomNotification;
+
+public interface CustomNotificationResponse {
+    void onResponse(CustomNotification customNotification);
+}
