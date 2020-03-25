5ffd3646f0497fe8253742ba099f0ad54af74c78
ventura <41873918+ventura@users.noreply.github.com>
2020-03-25 12:35:04 +0100

    Versión 1.10.0.1     - VisorImagenesAdapter: orientacion de imagenes base64.

--- app/src/main/java/es/icp/icp_commons/Utils/Utils.java
+++ app/src/main/java/es/icp/icp_commons/Utils/Utils.java
@@ -4,8 +4,13 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.util.Base64;
 import android.util.DisplayMetrics;
+
+import java.io.ByteArrayOutputStream;
 
 public class Utils {
 
@@ -29,7 +34,20 @@ public class Utils {
         return false;
     }
 
-    public static byte[] convertBase64ToByteArray(String base64) {
-        return Base64.decode(base64, Base64.DEFAULT);
+    public static byte[] convertBase64ToByteArray(String cadena) {
+        String[] cadenaSplit = cadena.split("\\|");
+        String base64 = cadenaSplit[0];
+        int degrees = Integer.parseInt(cadenaSplit[1]);
+        byte[] bytes = Base64.decode(base64, Base64.DEFAULT);
+
+        Bitmap storedBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, null);
+
+        Matrix mat = new Matrix();
+        if (degrees == 90 || degrees == 270) mat.postRotate(degrees);
+        storedBitmap = Bitmap.createBitmap(storedBitmap, 0, 0, storedBitmap.getWidth(), storedBitmap.getHeight(), mat, true);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        storedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+
+        return stream.toByteArray();
     }
 }
