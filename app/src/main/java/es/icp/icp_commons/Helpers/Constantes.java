34940d9b15bf13334af7e9eef3cb82b0fca8ae27
ventura.delucas <ventura.delucas@icp.es>
2020-07-06 13:46:04 +0200

    Versión 1.11.2.4     - Modificado el tiempo de TIME_OUT y el NUM_RETRY de las llamadas al WS.

--- app/src/main/java/es/icp/icp_commons/Helpers/Constantes.java
+++ app/src/main/java/es/icp/icp_commons/Helpers/Constantes.java
@@ -10,8 +10,8 @@ public class Constantes {
     public static final int AUDIO_ERROR    = 1;
     public static final int AUDIO_OHOH     = 2;
 
-    public static final int NUM_RETRY = 3;
-    public static final int TIMEOUT   = 15000;
+    public static final int NUM_RETRY = 0;
+    public static final int TIMEOUT   = 10000;
 
     public static final int CAMERA_REQUEST_CODE = 7002;
     public static final int CODE_PERMISSIONS    = 4001;
