e4b9801b3f1e4ffa9cf235a696348974806a6546
ventura.delucas <ventura.delucas@icp.es>
2021-02-02 16:27:00 +0100

    Versión 2.1.0     - Mejora en la función de GeoTracking.

--- app/src/main/java/es/icp/icp_commons/Objects/Coordenada.java
+++ app/src/main/java/es/icp/icp_commons/Objects/Coordenada.java
@@ -1,5 +1,7 @@
 package es.icp.icp_commons.Objects;
 
+import es.icp.logs.core.MyLog;
+
 public class Coordenada {
 
     private double longitud;
@@ -11,6 +13,19 @@ public class Coordenada {
     public Coordenada(double longitud, double latitud) {
         this.longitud = longitud;
         this.latitud  = latitud;
+    }
+
+    public double calcularDistanciaDesde(Coordenada comparar) {
+        double radioTierra = 6371000; //en metros
+        double dLat        = Math.toRadians(comparar.getLatitud() - this.latitud);
+        double dLng        = Math.toRadians(comparar.getLongitud() - this.longitud);
+        double sindLat     = Math.sin(dLat / 2);
+        double sindLng     = Math.sin(dLng / 2);
+        double va1 = Math.pow(sindLat, 2) + Math.pow(sindLng, 2) * Math.cos(Math.toRadians(this.latitud)) * Math.cos(Math.toRadians(comparar.getLatitud()));
+        double va2 = 2 * Math.atan2(Math.sqrt(va1), Math.sqrt(1 - va1));
+        double result = radioTierra * va2;
+        return result;
+
     }
 
     public double getLongitud() {
