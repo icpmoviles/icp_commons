80ba295b3a38689512bb75c4de708ad38c9c9188
ventura.delucas <ventura.delucas@icp.es>
2021-03-16 18:21:30 +0100

    Versión 2.2.4     - Fix PreferenciasHelper.

--- app/src/main/java/es/icp/icp_commons/PreferenciasHelper.java
+++ app/src/main/java/es/icp/icp_commons/PreferenciasHelper.java
@@ -24,14 +24,14 @@ public class PreferenciasHelper {
         SharedPreferences.Editor editor = sp.edit();
         if (object instanceof String) {
             editor.putString(key, (String) object);
+        }else if (object instanceof Long) {
+            editor.putInt(key, (Integer) object);
         } else if (object instanceof Integer) {
             editor.putInt(key, (Integer) object);
         } else if (object instanceof Boolean) {
             editor.putBoolean(key, (Boolean) object);
         } else if (object instanceof Float) {
             editor.putFloat(key, (Float) object);
-        } else if (object instanceof Long) {
-            editor.putLong(key, (Long) object);
         } else {
             editor.putString(key, object.toString());
         }
@@ -51,14 +51,14 @@ public class PreferenciasHelper {
 
         if (defaultObject instanceof String) {
             return sp.getString(key, (String) defaultObject);
+        }else if (defaultObject instanceof Long) {
+            return sp.getLong(key, (Long) defaultObject);
         } else if (defaultObject instanceof Integer) {
             return sp.getInt(key, (Integer) defaultObject);
         } else if (defaultObject instanceof Boolean) {
             return sp.getBoolean(key, (Boolean) defaultObject);
         } else if (defaultObject instanceof Float) {
             return sp.getFloat(key, (Float) defaultObject);
-        } else if (defaultObject instanceof Long) {
-            return sp.getLong(key, (Long) defaultObject);
         }
 
         return null;
