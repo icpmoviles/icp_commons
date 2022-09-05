2439910c6bb39b19d1fb30dc2080b3f0b0fe713f
gonzalo.racero <gonzalo.racero@icp.es>
2022-09-05 12:09:32 +0200

    Implementación de imagegallery y de unas animaciones

---
+++ app/src/main/java/es/icp/icp_commons/imageGallery/ImagenViewHolder.kt
@@ -0,0 +1,115 @@
+package es.icp.icp_commons.imageGallery
+
+import android.app.Activity
+import android.content.Intent
+import android.util.DisplayMetrics
+import android.view.View
+import android.widget.RelativeLayout
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultCaller
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+import androidx.lifecycle.LiveData
+import androidx.recyclerview.selection.ItemDetailsLookup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import es.icp.icp_commons.Camara.Camara
+import es.icp.icp_commons.R
+import es.icp.icp_commons.databinding.ImagenViewholderBinding
+
+class ImagenViewHolder(
+
+    private val activity                : Activity,
+
+    private val view                    : View,
+    private val binding                 : ImagenViewholderBinding,
+    private val numberOfCols            : Int,
+    private val alternativeImage        : Int?,
+    private val isPrevisualizable       : Boolean,
+    private val selectionTrackerFilled  : Int?,
+    private val selectionTrackerUnfilled: Int?
+
+) : RecyclerView.ViewHolder(view) {
+
+    fun bind(
+        imagen: String,
+        posicionImagen: Int,
+        longitudTotal: Int,
+        isSelected: Boolean,
+        isInDeleteMode: LiveData<Boolean>
+    ) = with(binding) {
+
+        // Ajusto el tamaño del viewholder de manera proporcial al numero de vistas del gridrecycler
+        val displayMetrics: DisplayMetrics = view.context.resources.displayMetrics
+        layoutImageContainer.layoutParams = RelativeLayout.LayoutParams(
+            displayMetrics.widthPixels / numberOfCols,
+            displayMetrics.widthPixels / numberOfCols
+        )
+
+        // En caso de que se haya seteado una imagen alternativa...
+        alternativeImage?.let {
+            Glide.with(view.context)
+                .load(imagen)
+                .placeholder(it)
+                .into(binding.imagen)
+        } ?: run {
+            Glide.with(view.context)
+                .load(imagen)
+                .into(binding.imagen)
+        }
+
+        isInDeleteMode.observeForever {
+            if (it) {
+                marcarParaBorrar.visibility = View.VISIBLE
+            } else {
+                marcarParaBorrar.visibility = View.GONE
+            }
+        }
+
+        // Reemplazo los iconos de borrado por defecto en caso de haber metido iconos personalizados
+        val trackerFilledIcon = selectionTrackerFilled ?: R.drawable.ic_check_filled
+        val trackerUnfilledIcon = selectionTrackerUnfilled ?: R.drawable.ic_check_unfilled
+
+        if (isSelected) {
+            marcarParaBorrar.background = ContextCompat.getDrawable(view.context, trackerFilledIcon)
+        } else {
+            marcarParaBorrar.background = ContextCompat.getDrawable(view.context, trackerUnfilledIcon)
+        }
+
+        // En caso de que sea previsualizable
+        if (isPrevisualizable) {
+
+            layoutImageContainer.setOnClickListener {
+
+                val intent = Intent(view.context, ImageGalleryPrevisualizationActivity::class.java)
+                val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                    activity,
+                    binding.imagen,
+                    SHARED_ELEMENT_NAME
+                )
+
+                intent.putExtra(RUTA_IMAGEN_INTENT, imagen)
+                intent.putExtra(IMAGE_TITLE_INTENT, "Imagen $posicionImagen de $longitudTotal")
+
+                if (alternativeImage != null) {
+                    intent.putExtra(RUTA_IMAGEN_ALTERNATIVA, alternativeImage)
+                }
+
+                startActivity(view.context, intent, options.toBundle())
+            }
+
+        }
+
+    }
+
+    fun getItem(): ItemDetailsLookup.ItemDetails<Long> =
+        object : ItemDetailsLookup.ItemDetails<Long>() {
+            override fun getPosition(): Int = absoluteAdapterPosition
+            override fun getSelectionKey(): Long = itemId
+        }
+
+}
---
+++ pruebas/src/main/java/es/icp/pruebas_commons/implementaciones/ImageGalleryImplementationActivity.kt
@@ -0,0 +1,72 @@
+package es.icp.pruebas_commons.implementaciones
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import es.icp.icp_commons.imageGallery.ImageGallery
+import es.icp.pruebas_commons.KotlinActivity
+import es.icp.pruebas_commons.R
+import es.icp.pruebas_commons.databinding.ActivityRecyclerSelectorExampleBinding
+
+class ImageGalleryImplementationActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityRecyclerSelectorExampleBinding
+    private lateinit var imageGallery : ImageGallery
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRecyclerSelectorExampleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupListeners()
+        setUpGaleriaDeImagenes()
+    }
+
+    private fun setUpGaleriaDeImagenes() {
+
+        // Esta string se utiliza como identificador en el tracker
+        // Pueden usuarse imágenes o uris correspondientes a rutas del teléfono
+        val elementosDePrueba = arrayListOf(
+            "https://picsum.photos/200",
+            "https://picsum.photos/300",
+            "https://picsum.photos/400",
+            "https://picsum.photos/500",
+            "https://picsum.photos/600",
+            "https://picsum.photos/700",
+            "https://picsum.photos/800",
+            "https://picsum.photos/900",
+            "IMAGEN ERRONEA A PROPÓSITO"
+        )
+
+        imageGallery = ImageGallery(
+            mActivity = this,
+            mContext = this,
+            recyclerView = binding.recycler,
+            listaDeImagenes = elementosDePrueba
+        )
+        .setCustomSelectionTrackerFilledIcon(R.drawable.dx_default_icon)   // El icono en caso de que la imagen esté seleccionada
+        .setCustomSelectionTrackerUnfilledIcon(R.drawable.ic_camara_focus) // El icono en caso de que la imagen no esté seleccionada
+        .setDefaultImageDrawable(R.drawable.circled_orange_button)         // En caso de que no exista la imagen se pondrá este circulo naranja como alternativa
+        .setPrevisualizable(true)                                          // Permite previsualizar la imagen al presionar sobre ellas
+        .setColumns(4)                                                     // Número de columnas
+        .setBackgroundColor(R.color.colorAccent)                           // Color de fondo del recycler
+        .drawGallery()                                                     // Dibuja la galería
+
+        imageGallery.addImage("https://picsum.photos/1000")          // Añade una imagen a la galería una vez ya creada de forma dinámica
+
+        // Esto debe tirar excepcion
+        // imageGallery.setColumns(12)
+
+    }
+
+    private fun setupListeners() = with(binding) {
+        btnVolverAKtAtivityDesdeRecyclerSelector.setOnClickListener {
+            val intent = Intent(
+                this@ImageGalleryImplementationActivity,
+                KotlinActivity::class.java
+            )
+            startActivity(intent)
+            finish()
+        }
+    }
+
+}
