4cc430601cf5bf145e3cd4aa639df8f35b8a369f
david.pits <david.pits@icp.es>
2024-04-10 14:08:15 +0200

    Añadido metodo para mostrar el dialog de novedades de verión

--- core/src/main/java/es/icp/icp_commons/Utils/UtilsKt.kt
+++ core/src/main/java/es/icp/icp_commons/Utils/UtilsKt.kt
@@ -4,6 +4,12 @@ import android.app.Activity
 import android.app.ActivityManager
 import android.content.Context
 import android.util.Log
+import android.view.LayoutInflater
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import es.icp.icp_commons.R
 
 object UtilsKt {
     fun isMyServiceRunning(serviceClass: Class<*>, mActivity: Activity): Boolean {
@@ -17,5 +23,51 @@ object UtilsKt {
         }
         Log.i("Service status", "Not running")
         return false
+    }
+
+    fun setUpDialog(description: String, context: Context, onAceptar: () -> Unit) {
+
+        // Padding del ul para separar de los bordes la lista
+        // Margin del li para separar los elementos de la lista entre sí
+        val htmlString = """
+        <style>
+            ul {
+                padding: 0 25px;
+            }
+
+            li {
+                margin-bottom: 10px;
+            }
+        </style>
+            $description
+    """.trimIndent()
+
+        // Creamos el dialog
+        val dialog = MaterialAlertDialogBuilder(context)
+            .setCancelable(false)
+            .create()
+
+        // Creamos la vista del dialog y asignamos a una variable el WebView
+        val dialogView =
+            LayoutInflater.from(context).inflate(R.layout.update_dialog_layout, null)
+        val webViewMessage = dialogView.findViewById<WebView>(R.id.webViewMessage)
+
+        webViewMessage.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                // Mostramos el dialog después de que la página se ha cargado completamente
+                dialog.show()
+            }
+        }
+
+        // Cargamos la vista HTML en el WebView
+        webViewMessage.loadDataWithBaseURL(null, htmlString, "text/html", "UTF-8", null)
+
+        // Asignamos la vista al dialog
+        dialog.setView(dialogView)
+
+        dialogView.findViewById<Button>(R.id.buttonAccept).setOnClickListener {
+            onAceptar()
+            dialog.dismiss()
+        }
     }
 }
